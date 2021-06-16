2360a321e8b56a9fe484d93e363ddbc08de90ea8
tangkaiqiang <justforgit@163.com>
2021-06-16 10:21:48 +0800

    feat:textarea style

---
+++ src/pages/form/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <view class="form-container">
+    <view>Form</view>
+    <view class="form-item">
+      <view class="form-label">说明</view>
+      <view class="form-input">
+        <input placeholder="请输入说明" />
+      </view>
+    </view>
+    <view class="form-item">
+      <view
+        class="form-label"
+        :style="{ paddingTop: ios ? '8px' : '0', width: ios ? '76px' : '80px' }"
+        >说明</view
+      >
+      <view class="form-input">
+        <textarea placeholder="请输入说明" class="txt"></textarea>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        ios: false,
+      }
+    },
+    mounted() {
+      const sys = uni.getSystemInfoSync()
+      console.log(sys.platform)
+      if (sys.platform === 'ios') {
+        this.ios = true
+      } else if (sys.platform === 'android') {
+        this.ios = false
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .form-container {
+    padding: 40rpx;
+    .form-item {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 20rpx 0;
+      border-bottom: 1rpx solid #e5e5e5;
+      .form-label {
+        width: 160rpx;
+      }
+      .form-input {
+        flex: 1;
+      }
+      .txt {
+        height: 200rpx;
+        max-width: 500rpx;
+      }
+      &:last-child {
+        align-items: flex-start;
+      }
+    }
+  }
+</style>
